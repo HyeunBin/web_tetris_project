280d43769058f3e7865018d14f3012464161f932
HyeunBin <tlsgusqls120@naver.com>
2022-09-26 17:31:43 +0900

    fixed bug and make line clear

--- make_board.js
+++ make_board.js
@@ -1,6 +1,8 @@
 var map = new Array(20);
 var piece = new Array(7);
 var block;
+var canvas;
+var score = 0;
 /*
 각종 블럭들
 shpae[i] = [y, x]
@@ -77,7 +79,8 @@ class _Z{
   }
 };
 
-function board(canvas){
+function board(_canvas){
+  canvas = _canvas;
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
 
@@ -98,17 +101,23 @@ function board(canvas){
   piece = [_I, _L, _J, _O, _S, _T, _Z];
 }
 
-function fall(canvas){
-  for(var i = 0; i < block.now.length; i++) if(block.now[i][0] + 1 == 20) return first_block_fall(canvas);
+function fall(){
+  for(var i = 0; i < block.now.length; i++) if(block.now[i][0] + 1 == 20) {
+    check_line();
+    return first_block_fall(canvas);
+  }
   for(var i = 0; i < block.check_down.length; i++){
     if(map[block.check_down[i][0] + 1][block.check_down[i][1]] == 1) {
       console.log("hall");
       console.log(block.check_down[i][0] + 1);
       console.log(block.check_down[i][1]);
+      check_line();
       return first_block_fall(canvas);
     }
     else block.check_down[i][0] += 1;
   }
+  for(var i = 0; i < block.check_left.length; i++) block.check_left[i][0] += 1;
+  for(var i = 0; i < block.check_right.length; i++) block.check_right[i][0] += 1;
 
   var ctx = canvas.getContext('2d');
   ctx.fillStyle = "white";
@@ -124,10 +133,10 @@ function fall(canvas){
   }
   //console.log(map[0]);
   //console.log(block.now)
-  setTimeout(fall, 500, canvas);
+  setTimeout(fall, 100, canvas);
 }
 
-function first_block_fall(canvas){
+function first_block_fall(){
   block = new piece[Math.floor(Math.random() * 7)]();
   console.log(block.name)
   for(var i = 0; i < block.now.length; i++){
@@ -143,13 +152,13 @@ function first_block_fall(canvas){
     ctx.fillRect(block.now[i][1] * 20 + 1, block.now[i][0] * 20 + 1, 19, 19);
   }
   console.log(map);
-  setTimeout(fall, 500, canvas);
+  setTimeout(fall, 100, canvas);
 }
 
 function key_down(e){
   switch(e.keyCode) {
     case 37: //left
-      for(var i = 0; i < block.check_left.length; i++) if(map[block.check_left[i][0][block.check_left[i][1]] - 1] == 1) return;
+      for(var i = 0; i < block.check_left.length; i++) if(map[block.check_left[i][0]][block.check_left[i][1] - 1] == 1) return;
       for(var i = 0; i < block.now.length; i++) if(block.now[i][1] - 1 < 0) return;
 
       for(var i = 0; i < block.check_down.length; i++) block.check_down[i][1] -= 1;
@@ -164,19 +173,60 @@ function key_down(e){
       }
       ctx.fillStyle = "green";
       for(var i = 0; i < block.now.length; i++){
-        map[block.now[i][0]][block.now[i][1] - 1] = 1;
         block.now[i][1] -= 1;
+        map[block.now[i][0]][block.now[i][1]] = 1;
         ctx.fillRect(block.now[i][1] * 20 + 1, block.now[i][0] * 20 + 1, 19, 19);
       }
       break;
-    case 38: //up
-
-      break;
     case 39: //right
+      for(var i = 0; i < block.check_right.length; i++) if(map[block.check_right[i][0]][block.check_right[i][1] + 1] == 1) return;
+      for(var i = 0; i < block.now.length; i++) if(block.now[i][1] + 1 > 9) return;
 
+      for(var i = 0; i < block.check_down.length; i++) block.check_down[i][1] += 1;
+      for(var i = 0; i < block.check_left.length; i++) block.check_left[i][1] += 1;
+      for(var i = 0; i < block.check_right.length; i++) block.check_right[i][1] += 1;
+
+      var ctx = document.getElementById('canvas').getContext('2d');
+      ctx.fillStyle = "white";
+      for(var i = 0; i < block.now.length; i++){
+        map[block.now[i][0]][block.now[i][1]] = 0;
+        ctx.fillRect(block.now[i][1] * 20 + 1, block.now[i][0] * 20 + 1, 19, 19);
+      }
+      ctx.fillStyle = "green";
+      for(var i = 0; i < block.now.length; i++){
+        block.now[i][1] += 1;
+        map[block.now[i][0]][block.now[i][1]] = 1;
+        ctx.fillRect(block.now[i][1] * 20 + 1, block.now[i][0] * 20 + 1, 19, 19);
+      }
       break;
     case 40: //down
 
       break;
+  }
+}
+
+function check_line(){
+  var ctx = canvas.getContext('2d');
+  for(var i = 19; i >= 0; i--){
+    var chk = true;
+    for(var j = 0; j < 10; j++) if(map[i][j] != 1){
+      chk = false;
+      break;
+    }
+    if(chk){
+      console.log("dlec");
+      score += 1;
+      for(var k = i; k >= 1; k--){
+        for(var j = 0; j < 10; j++){
+          ctx.fillStyle = "white";
+          ctx.fillRect(j * 20 + 1, k * 20 + 1, 19, 19);
+          map[k][j] = map[k-1][j];
+          if(map[k][j] == 1){
+            ctx.fillStyle = "green";
+            ctx.fillRect(j * 20 + 1, k * 20 + 1, 19, 19);
+          }
+        }
+      }
+    }
   }
 }
